7d0b4384dc8ea31b7d419b761500a3e003f52da5
George Bobrov <skivvvvy@gmail.com>
2021-03-10 00:42:22 +0200

    Change " to '

--- SizeInScreens.js
+++ SizeInScreens.js
@@ -1,6 +1,6 @@
 /*global chrome*/
 var mainConteiner = document.body;
-if (document.URL.startsWith("https://www.youtube.com/"))
+if (document.URL.startsWith('https://www.youtube.com/'))
 	mainConteiner = document.querySelector('ytd-app')
 
 var idAddresses = 'Addresses';
@@ -10,24 +10,24 @@ if (!observer)
 	addObservers();
 
 function addObservers() {
-	window.addEventListener("resize", windowResize);
-	window.addEventListener("scroll", windowResize);
+	window.addEventListener('resize', windowResize);
+	window.addEventListener('scroll', windowResize);
 	observer = new ResizeObserver(windowResize);
 	observer.observe(mainConteiner);
 }
 
 function windowResize(ev) {
 	// if (ev instanceof Event) {
-	// 	if (ev.type == "resize")
-	// 		console.log("window.resize", window.innerHeight);
-	// 	if (ev.type == "scroll")
-	// 		console.log("window.scroll", window.pageYOffset);
+	// 	if (ev.type == 'resize')
+	// 		console.log('window.resize', window.innerHeight);
+	// 	if (ev.type == 'scroll')
+	// 		console.log('window.scroll', window.pageYOffset);
 	// }
 	// if (ev instanceof Array)
 	// 	for (const el of ev)
-	// 		console.log("body.resize", el.contentRect.height)
+	// 		console.log('body.resize', el.contentRect.height)
 
-	let SizeInScreens = document.querySelectorAll("#SizeInScreens");
+	let SizeInScreens = document.querySelectorAll('#SizeInScreens');
 	if (SizeInScreens.length == 0) return;
 
 	let heightWindow = window.innerHeight;
@@ -35,7 +35,7 @@ function windowResize(ev) {
 	let heightBodyAll = mainConteiner.scrollHeight;
 
 	for (const el of SizeInScreens)
-		el.innerText = Math.ceil(curOffset/heightWindow) + " / " + Math.round(heightBodyAll/heightWindow)
+		el.innerText = Math.ceil(curOffset/heightWindow) + ' / ' + Math.round(heightBodyAll/heightWindow)
 }
 
 
@@ -45,28 +45,28 @@ windowResize();
 
 
 function createSizeInScreensElement() {
-	console.log("Create SizeInScreens element");
-	let div = document.createElement("div");
-	div.id = "floatSizeInScreens";
-	div.style.position = "fixed";
+	console.log('Create SizeInScreens element');
+	let div = document.createElement('div');
+	div.id = 'floatSizeInScreens';
+	div.style.position = 'fixed';
 	div.style.zIndex = 9999;
-	// div.style.border = "1px solid lightgrey";
-	div.style.backgroundColor = "white";
-	div.style.padding = "0 4px";
-	div.style.right = 15 + "px";
-	div.style.top = 15 + "px";
-	div.style.userSelect = "none";
+	// div.style.border = '1px solid lightgrey';
+	div.style.backgroundColor = 'white';
+	div.style.padding = '0 4px';
+	div.style.right = 15 + 'px';
+	div.style.top = 15 + 'px';
+	div.style.userSelect = 'none';
 	div.draggable = true;
 
-	let span = document.createElement("span");
-	span.innerHTML = "0";
-	span.id = "SizeInScreens";
+	let span = document.createElement('span');
+	span.innerHTML = '0';
+	span.id = 'SizeInScreens';
 
 	div.appendChild(span);
 
-	let divButtons = document.createElement("div");
-	divButtons.id = "SISButtons";
-	divButtons.style.display = "none"
+	let divButtons = document.createElement('div');
+	divButtons.id = 'SISButtons';
+	divButtons.style.display = 'none'
 
 	const buttonStyle = `
 		font-family: Courier, monospace;
@@ -76,22 +76,22 @@ function createSizeInScreensElement() {
 		border: revert;`;
 
 	// Added buttons for automatic launch on any given site
-	let buttonAdd = document.createElement("button");
-	buttonAdd.innerHTML = "+";
-	buttonAdd.title = "Always show SizeInScreens on this site";
+	let buttonAdd = document.createElement('button');
+	buttonAdd.innerHTML = '+';
+	buttonAdd.title = 'Always show SizeInScreens on this site';
 	buttonAdd.style = buttonStyle;
 	divButtons.appendChild(buttonAdd);
 
-	let buttonRemove = document.createElement("button");
-	buttonRemove.innerHTML = "-";
-	buttonRemove.style.display = "none";
+	let buttonRemove = document.createElement('button');
+	buttonRemove.innerHTML = '-';
+	buttonRemove.style.display = 'none';
 	buttonRemove.title = "Don't show SizeInScreens on this site";
 	buttonRemove.style = buttonStyle;
 	divButtons.appendChild(buttonRemove);
 
-	let buttonClose = document.createElement("button");
-	buttonClose.innerHTML = "✖";
-	buttonClose.title = "Close SizeInScreens";
+	let buttonClose = document.createElement('button');
+	buttonClose.innerHTML = '✖';
+	buttonClose.title = 'Close SizeInScreens';
 	buttonClose.style = buttonStyle + 'color: coral; margin-inline-start: 4%;';
 	divButtons.appendChild(buttonClose);
 	buttonClose.onclick = function(event) {
@@ -103,16 +103,16 @@ function createSizeInScreensElement() {
 
 	let timerMouseEnter;
 	div.onmouseenter = function(event) {
-		span.style.cursor = "move";
+		span.style.cursor = 'move';
 		timerMouseEnter = setTimeout(function() {
 			checkAddressInSavedAddresses();
-			divButtons.style.display = "block"
-			span.style.cursor = "";
+			divButtons.style.display = 'block'
+			span.style.cursor = '';
 		}, 500);
 	}
 
 	div.onmouseleave = function(event) {
-		divButtons.style.display = "none";
+		divButtons.style.display = 'none';
 		clearTimeout(timerMouseEnter);
 	}
 
@@ -129,7 +129,7 @@ function createSizeInScreensElement() {
 			addresses.push(newAddr)
 
 			chrome.storage.local.set({ [idAddresses]: addresses }, function() {
-				console.info("SizeInScreens: Saved new Address " + newAddr);
+				console.info('SizeInScreens: Saved new Address ' + newAddr);
 				checkAddressInSavedAddresses();
 			});
 		});
@@ -145,14 +145,14 @@ function createSizeInScreensElement() {
 			for (const addr of addresses) {
 				if (document.URL.startsWith(addr) && !wasDeleted)
 					wasDeleted = confirm(`Don't show SizeInScreens if URL address starts like this 
-"${addr}" ?`);
+'${addr}' ?`);
 				else
 					newAddresses.push(addr);
 			}
 
 			if (wasDeleted)
 				chrome.storage.local.set({ [idAddresses]: newAddresses }, function() {
-					console.info("SizeInScreens: Address was deleted");
+					console.info('SizeInScreens: Address was deleted');
 					checkAddressInSavedAddresses();
 				});
 
@@ -170,8 +170,8 @@ function createSizeInScreensElement() {
 					break;
 				}
 
-			buttonAdd.style.display = savedAddress ? "none" : "";
-			buttonRemove.style.display = savedAddress ? "" : "none";
+			buttonAdd.style.display = savedAddress ? 'none' : '';
+			buttonRemove.style.display = savedAddress ? '' : 'none';
 		});
 	}
 }
@@ -184,7 +184,7 @@ floatSizeInScreens.onclick = function(event) {
 floatSizeInScreens.ondblclick = function(event) {
 	chrome.storage.local.get([idAddresses], function(data) {
 		let addresses = data[idAddresses] || [];
-		alert(addresses.join("\n"))
+		alert(addresses.join('\n'))
 	});
 }
 
@@ -194,8 +194,8 @@ var savedOffsetY;
 var SizeInScreensDrag = false;
 
 floatSizeInScreens.ondragstart = function(event) {
-	let SISButtons = this.querySelector("#SISButtons");
-	if ((SISButtons.style.display === "block") && (event.offsetY > SISButtons.offsetTop)) {
+	let SISButtons = this.querySelector('#SISButtons');
+	if ((SISButtons.style.display === 'block') && (event.offsetY > SISButtons.offsetTop)) {
 		event.preventDefault();  //cancel dragging by grabbing buttons
 		return;
 	}
@@ -214,18 +214,18 @@ floatSizeInScreens.ondragend = function(event) {
 	let newTop = this.offsetTop + movementY;
 
 	if (newLeft < window.visualViewport.width / 2) {
-		this.style.left = newLeft + "px";
-		this.style.right = "";
+		this.style.left = newLeft + 'px';
+		this.style.right = '';
 	} else {
-		this.style.right = window.visualViewport.width - newLeft - this.offsetWidth + "px";
-		this.style.left = "";
+		this.style.right = window.visualViewport.width - newLeft - this.offsetWidth + 'px';
+		this.style.left = '';
 	}
-	this.style.top = newTop + "px";
+	this.style.top = newTop + 'px';
 
 	SizeInScreensDrag = false;
 }
 
-window.addEventListener("dragover", windowDragower);
+window.addEventListener('dragover', windowDragower);
 
 function windowDragower(event) {
 	if (SizeInScreensDrag)
